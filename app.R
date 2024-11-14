47908613a2da17eee042ee9b1be2547c821fdfd2
Samuel Calderon <samuel.calderon@uarm.pe>
2024-11-14 17:39:12 -0500

    Initial commit

---
+++ app.R
@@ -0,0 +1,87 @@
+library(shiny)
+library(bslib)
+library(tidyverse)
+
+survey <- readr::read_csv("data/survey.csv") |> 
+  slice_sample(n = 5000, by = region)  
+
+ui <- page_sidebar(
+  
+  sidebar = sidebar(
+    selectInput(
+      inputId = "region",
+      label = "Seleccione region",
+      choices = discard(unique(survey$region), is.na)
+    ),
+    sliderInput(
+      inputId = "age",
+      label = "Seleccione edad máxima",
+      min = 10,
+      max = 100,
+      value = 100,
+      step = 10
+    )
+  ),
+  
+  card(
+    max_height = "50%",
+    tableOutput("table")
+  ),
+  
+  layout_columns(
+    col_widths = c(4, 4, 4),
+    
+    card(
+      plotOutput("histogram")
+    ),
+    card(
+      full_screen = TRUE,
+      plotOutput("by_transport")
+    ),
+    card(
+      full_screen = TRUE,
+      plotOutput("by_type")
+    )
+    
+  )
+  
+)
+
+server <- function(input, output, session) {
+  output$table <- renderTable({
+    survey |> 
+      filter(region == input$region) |> 
+      filter(age <= input$age)
+  })
+  
+  output$histogram <- renderPlot({
+    survey |> 
+      filter(region == input$region) |> 
+      filter(age <= input$age) |> 
+      ggplot(aes(temps_trajet_en_heures)) +
+      geom_histogram(bins = 20) +
+      theme_light()
+  })
+  
+  output$by_transport <- renderPlot({
+    survey |> 
+      filter(region == input$region) |> 
+      filter(age <= input$age) |> 
+      ggplot(aes(temps_trajet_en_heures)) +
+      geom_histogram(bins = 20) +
+      facet_wrap(~transport) +
+      theme_light()
+  })
+  
+  output$by_type <- renderPlot({
+    survey |> 
+      filter(region == input$region) |> 
+      filter(age <= input$age) |> 
+      ggplot(aes(temps_trajet_en_heures)) +
+      geom_histogram(bins = 20) +
+      facet_wrap(~type) +
+      theme_light()
+  })
+}
+
+shinyApp(ui, server)
