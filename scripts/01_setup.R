381ee76ef29ddebf8c0eab50e64fd35cc3435888
Samuel Calderon <samuel.calderon@uarm.pe>
2024-11-17 19:04:43 -0500

    add DT, plotly, future deps

--- scripts/01_setup.R
+++ scripts/01_setup.R
@@ -2,7 +2,7 @@ install.packages("pak")
 
 pak::pkg_install(
   c(
-    c("shiny", "bslib", "tidyverse", "data.table", "fst", "arrow", "profvis"), # For app
+    c("shiny", "bslib", "tidyverse", "data.table", "fst", "arrow", "profvis", "DT", "plotly", "future"), # For app
     c("rsconnect", "cpp11", "progress") # For deployment
   )
 )
